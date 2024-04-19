64b970d1b1d0edd515102c58cb2e3374c994c4fa
jerquinigo <jonathanerquinigo@pursuit.org>
2024-04-19 00:15:11 -0400

    added styling

--- src/main/java/ulil/WindowOptions.java
+++ src/main/java/ulil/WindowOptions.java
@@ -12,12 +12,37 @@ public class WindowOptions extends JFrame implements ActionListener {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // create a label
-        JTextArea textArea = new JTextArea("This is a multiline text area.");
-        textArea.setEditable(false); // Set editable to false if you only want to display text
-        JScrollPane scrollPane = new JScrollPane(textArea); // Add a scroll pane if needed
+        JTextPane textPane = new JTextPane();
+//        JTextArea textArea = new JTextArea("<html>" +
+//                "<h1>Moose Alert</h1> " +
+//                "<i>Italic Text</i> <font color='red'>Red Text</font>" +
+//                "</html>\"");
+//        textArea.setEditable(false); // Set editable to false if you only want to display text
+//        textArea.set
+        textPane.setContentType("text/html");
+        textPane.setText("<html>" +
+                "<head> " +
+                "<style> " +
+                 ".flex-item { " +
+             "background-color: lightblue; " +
+            "padding: 10px; " +
+        "} " +
+  "</style> " +
+                "</head>" +
+                "<div style=display: flex; justify-content: center; align-items: center;>" +
+                    "<h1>Moose Alert</h1> " +
+                   "<font color='red'>Time to take Moose out for a walk</font>" +
+                "<div> " +
+                "<div style=display: flex; justify-content: center; align-items: center;> " +
+                "<img src='https://media.tenor.com/ZrQUcJZ660kAAAAM/rico-ricowestie.gif'> " +
+                "</div>" +
+                "</html>\"");
+
+        JScrollPane scrollPane = new JScrollPane(textPane); // Add a scroll pane if needed
         frame.add(scrollPane, BorderLayout.CENTER);
 
         frame.setSize(800, 600);
+
 
 //        PopupFactory popupFactory = new PopupFactory();
 //
